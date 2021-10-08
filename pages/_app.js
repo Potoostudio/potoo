b5f5982f7f80c8a6eea364383b741b2d3be4a57c
Potoostudio <89357284+Potoostudio@users.noreply.github.com>
2021-10-08 16:51:18 -0600

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,32 +4,31 @@ import Layout from '../comps/Layout'
 import {ThemeProvider} from 'styled-components'
 import {useState} from 'react'
 import { darkTheme, lightTheme, GlobalStyles } from '../ThemeConfig'
-
-
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from './lib/ga'
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
 
-  const [isActive, setActive] = useState("false");
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
   
-  const [theme, setTheme] = useState("dark");
-  
-  
-  const toggleTheme = () => {
-    theme == 'dark' ? setTheme('light') : setTheme('dark')
-    setActive(!isActive);
-}
-  
-  return (
-    <ThemeProvider theme={theme =='dark' ? darkTheme : lightTheme}>
-      <GlobalStyles/>
-        <Layout>
-          <button className={isActive ? "mode-btn dark" : "mode-btn light"} onClick={toggleTheme}><span className="mode-icon"></span></button>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
-    
-  )
 }
 
 export default MyApp
