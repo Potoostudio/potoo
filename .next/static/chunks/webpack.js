d9253e7169760c9ab324166cfcadf558c3ab9971
Potoostudio <89357284+Potoostudio@users.noreply.github.com>
2021-11-10 15:58:19 -0700

    new favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f0b74020331c715ac48"; }
+/******/ 		__webpack_require__.h = function() { return "862fb4b98a9cc47b0172"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
