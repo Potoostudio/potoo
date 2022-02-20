424c3cf679e4480f5d50077fe2a46531c05f2133
potoostudio <ideas@potoo.studio>
2022-02-19 20:33:28 -0700

    another css update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e0d387c24b632337272"; }
+/******/ 		__webpack_require__.h = function() { return "9deccbc11c8ed55a430a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
