809ae9ac869d51ef0ca19701a5de5f9185a82c86
potoostudio <ideas@potoo.studio>
2022-02-22 11:49:28 -0700

    iframe styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eddee69bd00e41e9e9f3"; }
+/******/ 		__webpack_require__.h = function() { return "442e203be77ff8ea8211"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
