80c8ebd19605047cee0c9d0557ef9be50d981ba0
potoostudio <ideas@potoo.studio>
2022-04-05 10:46:59 -0600

    copy of design page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d6c19371c4cb92043f38"; }
+/******/ 		__webpack_require__.h = function() { return "a63a1f761653983d0cbc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/about.js
+++ pages/about.js
@@ -20,7 +20,7 @@ function Design() {
             </Head>
             
             
-            <div className="container about-container pt-5 pt-md-5">
+            <div className="container about-container pt-5 pt-md-5 px-4 px-md-0">
               <div className="row justify-content-start">
                   <div className="col-md-12 col-11">
                     <h1 className="we-are-potoo-h1">
@@ -35,22 +35,14 @@ function Design() {
                   </h3>
                 </div>
               </div>
-              <div className="row justify-content-center logo-concept-row">
-                <div className="col-md-10 col-lg-9 col-11">
+              <div className="row justify-content-start logo-concept-row">
+                <div className="col-md-11 col-11">
                   <h1 className="logo-concept-h1">
                     The concept of our logo
                   </h1>
                   <div className="logo-concept-box">
-                    <div className="logo-concept-box-image">
-                      <Image
-                        src="/logo-192x192.png"
-                        width="100%"
-                        height="110px"
-                        alt="potoo logo"
-                      />
-                    </div>
                     <div className="logo-concept-box-content">
-                      <ul className="list pt-md-2 pt-0">
+                      <ul className="list pt-md-3 pt-0">
                         <li className="item">
                           Huge eyes are capable of spotting impressions 
                         </li>
@@ -64,10 +56,10 @@ function Design() {
               </div>
               <div className="row justify-content-start content-row">
                 <div className="col-11 col-md-11 d-lg-flex">
-                  <p className="normal-p">
+                  <p className="normal-p pl-0">
                     We have no interest in rubbish work. We personalize brands to create unique images.
                   </p>
-                  <p className="normal-p">
+                  <p className="normal-p pl-0">
                     We remember the concept of our logo and consider impressions with every project we take
                   </p>
                 </div>
