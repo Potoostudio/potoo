c0fe9c81f269345d09a0e2371bdc59ce455c85bf
potoostudio <ideas@potoo.studio>
2022-04-15 08:42:55 -0600

    update font link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef324c512fe82bce1320"; }
+/******/ 		__webpack_require__.h = function() { return "3687278660c1a573dd34"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.2743d9ae39e92d89120e.hot-update.js
+++ .next/static/webpack/webpack.2743d9ae39e92d89120e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a57daebc4d8811501bc1"; }
+/******/ 	__webpack_require__.h = function() { return "3687278660c1a573dd34"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
