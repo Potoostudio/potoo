002ad3ed276000a3ae4c5e1f7e9d28351410f56f
potoostudio <ideas@potoo.studio>
2022-06-08 09:37:59 -0600

    css query 280px for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "887ddc79421cb18b1479"; }
+/******/ 		__webpack_require__.h = function() { return "7c37aa3aa328b5f2b165"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
