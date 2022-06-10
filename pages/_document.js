d724c559fbb47a9091e29d0c7586b01517cab3b7
potoostudio <ideas@potoo.studio>
2022-06-10 12:57:38 -0600

    google tag account

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,25 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <!-- Google Tag Manager --> */}
+          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2DXR4D');`}} />
+
+          {/* <!-- Google Tag Manager --> */}
+          <noscript dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N2DXR4D');`}} />
+            
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2DXR4D"
+          height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+
+
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
