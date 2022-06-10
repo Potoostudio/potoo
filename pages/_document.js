f5d57ddf00c631663fc92776745a01f54b4079fc
potoostudio <ideas@potoo.studio>
2022-06-10 13:58:32 -0600

    analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -12,47 +12,9 @@ export default class MyDocument extends Document {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-N2DXR4D');`}} />
 
-          {/* <!-- Google Tag Manager --> */}
-          <noscript dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N2DXR4D');`}} />
-            
           {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2DXR4D"
           height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
-
-
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-JDP4HBPC05`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-JDP4HBPC05', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-          {/* <!-- Hotjar Tracking Code for https://www.potoo.studio --> */}
-          <script dangerouslySetInnerHTML={{__html: `(function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:2967831,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} />
-          {/* Google font link */}
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" 
-          />
         </Head>
         <body>
           <Main />
