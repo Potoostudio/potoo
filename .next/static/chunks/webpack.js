49c7e279c5c1129ece625147774e46da90da27cd
potoostudio <ideas@potoo.studio>
2022-12-28 18:42:45 -0700

    mobile still win

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd9281245b3ccbb4"; }
+/******/ 		__webpack_require__.h = function() { return "3129fb7ad83556c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
