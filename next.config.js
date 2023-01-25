a1831c1af5e7b6ab2ffda39a1ffb4a38301921fc
potoostudio <ideas@potoo.studio>
2023-01-25 14:53:23 -0700

    6.3

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-const path = require('path')
-
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  output: 'standalone',
-  distDir: 'build',
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
+    experimental: {
+      appDir: true,
+    },
+  };
