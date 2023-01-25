55a5cb689acebd26342fa1ad3d154ccf694a481e
potoostudio <ideas@potoo.studio>
2023-01-25 13:53:58 -0700

    potoo.2

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    async rewrites( ) {
-        return [ {
-            source: '/api/contact-handle',
-            destination: 'https://potoo-flax.vercel.app/api/contact-handle',
-        },
-        ]
-    },
-};
